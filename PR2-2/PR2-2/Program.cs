281a81a61006d444306ded0c636b280562251d5b
student <student@C251U27>
2020-09-18 11:02:37 +0300

    1

---
+++ PR2-2/PR2-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PR2_2
+{
+  using System;
+  using System.Collections;
+
+  namespace prakt4_01
+  {
+    class Program
+    {
+
+      static void Main(string[] args)
+      {
+        ArrayList myList = new ArrayList();
+        myList.Add("First");
+        myList.Add("Second");
+        myList.Add("Third");
+        myList.Add("Fourth");
+        foreach (string item in myList)
+        {
+          Console.WriteLine("Unsorted: {0}", item);
+        }
+        // Сортировка при помощи стандартного объекта сравнения 
+        myList.Sort();
+        foreach (string item in myList)
+        {
+          Console.WriteLine("   Sorted: {0}", item);
+        }
+
+      }
+
+    }
+  }
+}
